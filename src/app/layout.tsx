6062437819171aaed303034bc1a9fcf1d9dfb981
Saksham <sakshamsehrawet@gmail.com>
2024-05-17 15:15:45 +0530

    basic next app with auth

---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import NavBar from "@/components/NavBar";
+import { Toaster } from "@/components/ui/toaster";
+import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | Mota.atoM",
+    absolute: "Mota.atoM",
+  },
+  description:
+    "Something",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider>
+          <NavBar />
+          {children}
+          <Toaster />
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
